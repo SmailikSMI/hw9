307ca2b131c4c5ab5f5ad238315e4885429abd02
SmailikSMI <120740208+SmailikSMI@users.noreply.github.com>
2023-02-05 11:33:50 +0300

    Add files via upload

--- HoweWork9/src/by/teachmeskills/siniak/homework9/HomeWork9.java
+++ HoweWork9/src/by/teachmeskills/siniak/homework9/HomeWork9.java
@@ -6,22 +6,36 @@ import java.util.Scanner;
 
 public class HomeWork9 {
     public static void main(String[] args) {
-
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите символы: ");
+        System.out.println("Enter symbols: ");
         char[] string = scanner.nextLine().toCharArray();
-        System.out.println(string.length);
-        System.out.println("Введите номер задачи: ");
+        StringUtils.println(string);
+        System.out.println("Enter number task: ");
         int taskNumber = scanner.nextInt();
         switch (taskNumber) {
             case 2 -> {
                 boolean isBank = StringUtils.isBank(string);
-                System.out.println(isBank);
+                System.out.println("This string is bank?");
+                System.out.println("It's " + isBank);
 
             }
+            case 3 -> {
+                boolean isRussian = StringUtils.isRussian(string);
+                System.out.println("This string is Russian?");
+                System.out.println("It's " + isRussian);
+            }
+            case 4 -> {
+                Scanner sca = new Scanner(System.in);
+                System.out.println("Введите символы: ");
+                char[] subString = sca.nextLine().toCharArray();
+                boolean contains = StringUtils.contains(string, subString);
+                System.out.println("This string contenting second string?");
+                System.out.println("It's " + contains);
+            }
+            case 5 -> {
+                int digit = StringUtils.parseInt(string);
+                System.out.println("Your number is " + digit);
+            }
         }
-
-
     }
 }
--- HoweWork9/src/by/teachmeskills/siniak/homework9/util/StringUtils/StringUtils.java
+++ HoweWork9/src/by/teachmeskills/siniak/homework9/util/StringUtils/StringUtils.java
@@ -2,31 +2,81 @@ package by.teachmeskills.siniak.homework9.util.StringUtils;
 
 public class StringUtils {
 
-    public static void print (char[] string){
-        System.out.print(string);
-
-    }
-    public static void println (char[] string){
-        System.out.println(string);
-
-    }
-
-    public static boolean isBank (char[] string){
-        if(string.length==0){
-            return false;
+    public static void print(char[] string) {
+        System.out.print("Your string: ");
+        for (int j = 0; j < string.length; j++) {
+            System.out.print(string[j]);
         }
-        char Blank=0;
-        for (int j=0;j<string.length;j++) {
-            System.out.println("Loop");
-            if (string[j]==' '){
-                string[j]+=Blank;
-                System.out.println(Blank);
+    }
+
+    public static void println(char[] string) {
+        System.out.print("Your string: ");
+        for (int j = 0; j < string.length; j++) {
+            System.out.print(string[j]);
+        }
+        System.out.println();
+    }
+
+    public static boolean isBank(char[] string) {
+        if (string.length == 0) {
+            return true;
+        }
+        char space = 0;
+        for (int j = 0; j < string.length; j++) {
+            if (string[j] == ' ') {
+                space++;
             }
-
         }
-        return true;
+        return space == string.length;
     }
 
+    public static boolean isRussian(char[] string) {
+        int i = 0;
+        int space = 0;
+        for (int j = 0; j < string.length; j++) {
+            if (string[j] == ' ') {
+                space++;
+            }
+            if (string[j] > 'А' && string[j] < 'я' || string[j] == ' ') {
+                i++;
+            }
+            if (space == string.length) {
+                return false;
+            }
+            if (i == string.length) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public static boolean contains(char[] string, char[] subString) {
+        int i = 0;
+        char val1 = 0;
+        char val2 = 0;
+        for (int j = 0; j < string.length; j++) {
+            string[j] = val1;
+            subString[j] = val2;
+            if (val1 == val2) {
+                i++;
+            }
+            if (i == subString.length) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public static int parseInt(char[] string) {
+        int digit = 0;
+        for (int j = 0; j < string.length; j++) {
+            digit += (string[j] - '0') * (int) Math.pow(10, string.length - j - 1);
+        }
+        return digit;
+    }
 }
+
+
+
+
+
